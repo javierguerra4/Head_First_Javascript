ce06ea655681aff74af9f2696ccf86df3807ca80
Javier Guerra <javier.j.guerra4@gmail.com>
2021-10-26 07:12:47 -0500

    Created while loop for battleship

--- js/battleship.js
+++ js/battleship.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var location1 =3;
+var location1 = 3;
 
-var location2 =4;
+var location2 = 4;
 
-var location3 =5;
+var location3 = 5;
 
 var guess;
 
@@ -13,3 +13,25 @@ var hits = 0;
 var guesses = 0;
 
 var isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6) :");
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!");
+    } else {
+        guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+        }
+    }
+}
+
+// if (guess == location1) {
+//     hits = hits + 1;
+// } else if (guess == location2) {
+//     hits == hits + 1;
+//
+// } else if (guess == location3) {
+//     hits = hits + 1;
+// }
