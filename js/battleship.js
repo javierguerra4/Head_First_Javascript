b439523a3b0658499c78d69a8a75ca3ecdc46b1d
Javier Guerra <javier.j.guerra4@gmail.com>
2021-10-26 07:23:45 -0500

    Basic battleship code done

--- js/battleship.js
+++ js/battleship.js
@@ -22,16 +22,21 @@ while (isSunk == false) {
         guesses = guesses + 1;
 
         if (guess == location1 || guess == location2 || guess == location3) {
+            alert ("HIT!");
             hits = hits + 1;
+
+            if (hits == 3) {
+                isSunk = true;
+                alert ("You sank my battleship!");
+            }
+        } else {
+            alert("MISS");
         }
     }
+
 }
 
-// if (guess == location1) {
-//     hits = hits + 1;
-// } else if (guess == location2) {
-//     hits == hits + 1;
-//
-// } else if (guess == location3) {
-//     hits = hits + 1;
-// }
+var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3/guesses);
+
+alert(stats);
+
