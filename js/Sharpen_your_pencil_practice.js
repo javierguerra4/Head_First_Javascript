80994c89d8afcf7a1dd4f52b72a93aa4e421e7fd
Javier Guerra <javier.j.guerra4@gmail.com>
2021-10-29 14:19:35 -0500

    isValid was true

--- js/Sharpen_your_pencil_practice.js
+++ js/Sharpen_your_pencil_practice.js
@@ -1,5 +1,7 @@
 "use strict";
 
+//What is the value?
+
 var temp = 81;
 
 var willRain = true;
@@ -7,4 +9,11 @@ var willRain = true;
 var humid = (temp > 80 && willRain == true);
 
 console.log("The humidity is " + humid)
+
+//What is the value?
+var guess = 6;
+
+var isValid = (guess >= 0 && guess <= 6);
+
+console.log(isValid);
 
