ffeefbeb51c9cc143acb3f3ffbdcf3c2550e310f
Javier Guerra <javier.j.guerra4@gmail.com>
2021-11-01 22:25:38 -0500

    Created function to find area of triangle on first try

--- js/edabit_practice_problems.js
+++ js/edabit_practice_problems.js
@@ -15,3 +15,9 @@ function convert(minutes) {
 
 }
 console.log (convert(60));
+
+// Write a function that takes the base and height of a triangle and return its area.
+function triArea(base, height) {
+return ((base * height) / 2);
+}
+console.log(triArea(10, 10));
