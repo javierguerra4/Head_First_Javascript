e527667c6e1dbdee3f8013fa612f354a9cad5fe6
Javier Guerra <javier.j.guerra4@gmail.com>
2021-11-06 13:27:07 -0500

    Created return value for points

--- js/edabit_practice_problems.js
+++ js/edabit_practice_problems.js
@@ -58,3 +58,8 @@ function nameString(name){
 function sumPolygon(n) {
 return ((n - 2) * 180);
 }
+
+//You are counting points for a basketball game, given the amount of 2-pointers scored and 3-pointers scored, find the final points for the team and return that value.
+function points(twoPointers, threePointers) {
+return ((twoPointers * 2) + (threePointers * 3));
+}
