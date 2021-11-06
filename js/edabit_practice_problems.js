ec9387ee60ded14c341e25e926a573989b6c2c8a
Javier Guerra <javier.j.guerra4@gmail.com>
2021-11-06 13:17:14 -0500

    Removed extra equals

--- js/edabit_practice_problems.js
+++ js/edabit_practice_problems.js
@@ -42,3 +42,13 @@ return num + 1;
 function circuitPower(voltage, current) {
 return voltage * current;
 }
+
+//A student learning JavaScript was trying to make a function.
+// His code should concatenate a passed string name with string "Edabit" and store it in a variable called result.
+// He needs your help to fix this code.
+
+function nameString(name){
+    var b = "Edabit"
+    var result = name + b
+    return result;
+}
