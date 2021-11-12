9a3f27e9abede949da45aff2ab565d5933518c4c
Javier Guerra <javier.j.guerra4@gmail.com>
2021-11-12 08:56:42 -0600

    Practice: Completed lessThan100 function

--- js/edabit_practice_problems.js
+++ js/edabit_practice_problems.js
@@ -90,3 +90,15 @@ function giveMeSomething(a) {
 return "Something " + a;
 }
 console.log(giveMeSomething("is better than nothing right?"));
+
+// Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+
+function lessThan100(a, b) {
+if ((a + b) >= 100){
+    return false
+
+    } else{
+    return true
+};
+}
+console.log(lessThan100(99, 0));
