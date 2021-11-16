e7811e7d288554f880531424f8a4d81d2ab05b35
Javier Guerra <javier.j.guerra4@gmail.com>
2021-11-15 21:37:02 -0600

    Practice: Completed the comp function

--- js/edabit_practice_problems.js
+++ js/edabit_practice_problems.js
@@ -183,3 +183,17 @@ function calculateFuel(n) {
 }
 
 console.log(calculateFuel(2));
+
+//Create a function that takes two strings as arguments and return either true or false depending on whether the total number of characters in the first string is equal to the total number of characters in the second string.
+
+function comp(str1, str2) {
+ if (str1.length !== str2.length) {
+     return false;
+ } else{
+     return true;
+ }
+}
+
+console.log(comp("AB", "CD"));
+console.log(comp("ABC", "DE"));
+console.log(comp("hello", "edabit"));
