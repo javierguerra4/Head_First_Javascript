8a72fd83f4ace50a982e8ecc7333715f10478b3f
Javier Guerra <javier.j.guerra4@gmail.com>
2021-11-15 21:25:50 -0600

    Practice: Completed calculateFuel function

--- js/edabit_practice_problems.js
+++ js/edabit_practice_problems.js
@@ -169,3 +169,17 @@ console.log(lessThanOrEqualToZero(2));
 function minMax(arr) {
 
 }
+
+//A vehicle needs 10 times the amount of fuel than the distance it travels. However, it must always carry a minimum of 100 fuel before setting off.
+//
+// Create a function which calculates the amount of fuel it needs, given the distance.
+
+function calculateFuel(n) {
+    if (n * 10 < 100) {
+        return 100;
+    } else {
+        return (n * 10);
+    }
+}
+
+console.log(calculateFuel(2));
