2692bd3cad2bc878bd1050342f0e74f91f96109a
Javier Guerra <javier.j.guerra4@gmail.com>
2021-12-23 14:51:37 -0600

    Completed timeForMilkAndCookies funtion with assigit status :q

--- js/edabit_practice_problems.js
+++ js/edabit_practice_problems.js
@@ -287,3 +287,14 @@ function animals(chickens, cows, pigs) {
 return ((chickens * 2) + (cows * 4) + (pigs * 4));
 }
 console.log(animals(5, 2, 8))
+
+// Christmas Eve is almost upon us, so naturally we need to prepare some milk and cookies for Santa!
+// Create a function that accepts a Date object and returns true if it's Christmas Eve (December 24th) and false otherwise.
+// Keep in mind JavaScript's Date month is 0 based, meaning December is the 11th month while January is 0.
+//****How does instanceof work?
+function timeForMilkAndCookies(date) {
+    return date instanceof Date && date.getMonth() === 11 && date.getDate() === 24;
+
+}
+
+console.log(timeForMilkAndCookies(new Date(2025, 11, 24)))
