9180af408ec854efa3831170241f6b7371905169
Javier Guerra <javier.j.guerra4@gmail.com>
2022-01-05 16:26:51 -0600

    Completed practice function removeVowels

--- js/edabit_practice_problems.js
+++ js/edabit_practice_problems.js
@@ -386,3 +386,23 @@ console.log(sayHelloBye("Javier", 3))
 // sayHelloBye("Tomi", 0) ➞ "Bye Tomi"
 //
 // sayHelloBye("jose", 0) ➞ "Bye Jose"
+
+
+/*
+Create a function that takes a string and returns a new string with all vowels removed.
+removeVowels("I have never seen a thin person drinking Diet Coke.")
+➞ " hv nvr sn  thn prsn drnkng Dt Ck."
+
+removeVowels("We're gonna build a wall!")
+➞ "W'r gnn bld  wll!"
+
+removeVowels("Happy Thanksgiving to all--even the haters and losers!")
+➞ "Hppy Thnksgvng t ll--vn th htrs nd lsrs!"
+*/
+
+function removeVowels(str) {
+return str.replace(/[aeiou]/ig,'')
+}
+
+console.log(removeVowels("I have never seen a thin person drinking Diet Coke."))
+
